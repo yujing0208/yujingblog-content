92fe52dee01ea36e411d036461921099a19010b6
yujing0208 <2803673194@qq.com>
2026-07-26 12:36:03 +0800

    chore: clear anime and devices data records

--- data/devices.ts
+++ data/devices.ts
@@ -13,24 +13,4 @@ export type DeviceCategory = Record<string, Device[]> & {
 	自定义?: Device[];
 };
 
-export const devicesData: DeviceCategory = {
-	OnePlus: [
-		{
-			name: "OnePlus 13T",
-			image: "/images/device/oneplus13t.webp",
-			specs: "Gray / 16G + 1TB",
-			description: "Flagship performance, Hasselblad imaging, 80W SuperVOOC.",
-			link: "https://www.oneplus.com/cn/13t",
-		},
-	],
-	Router: [
-		{
-			name: "GL-MT3000",
-			image: "/images/device/mt3000.webp",
-			specs: "1000Mbps / 2.5G",
-			description:
-				"Portable WiFi 6 router suitable for business trips and home use.",
-			link: "https://www.gl-inet.cn/products/gl-mt3000/",
-		},
-	],
-};
+export const devicesData: DeviceCategory = {};

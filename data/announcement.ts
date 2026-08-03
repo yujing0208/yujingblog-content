618c8983ee0ee4fec213476d8458c9068ed8ebab
yujing0208 <2803673194@qq.com>
2026-08-03 13:51:47 +0800

    update content

--- data/announcement.ts
+++ data/announcement.ts
@@ -15,7 +15,7 @@ export interface AnnouncementData {
 
 export const announcementData: AnnouncementData = {
 	title: "",
-	content: "又换博客啦！欢迎访问YuJing的小站; 想访问旧站的可以点击下方[Learn More] 按钮",
+	content: "又换博客啦！欢迎访问YuJing的小站; 想访问旧站的可以点击下方「Learn More」按钮",
 	closable: true,
 	link: {
 		enable: true,

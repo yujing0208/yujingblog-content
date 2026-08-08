2b75c4152cc4a906f44049b3a816c0623629c065
余京 <2803673194@qq.com>
2026-08-08 11:14:21 +0800

    chore(editor): update data/anime.ts

--- data/anime.ts
+++ data/anime.ts
@@ -486,6 +486,26 @@ const localAnimeList: AnimeItem[] = [
 		link: "https://www.bilibili.com/bangumi/media/md8892",
 		progress: "",
 		totalEpisodes: ""
+	},
+	{
+		title: "冰菓 ",
+		status: "completed",
+		category: "anime",
+		rating: 9.8,
+		cover: "https://i0.hdslb.com/bfs/bangumi/image/f623059d4e370a5b09a7c27b5a035895b40d18f8.png@450w_600h.webp",
+		description: "在众多将要展开「玫瑰色」生活的高中生之中，本作的男主角折木奉太郎却是一个「灰色」的节能主义者。凡是没必要的事就不做，因为不想后悔，被人说是疏离、厌世也无所谓，因为这就是他的作风。这样的折木奉太郎，却因为姐姐的命令而进入了濒临废社的「古籍研究社」。研究社虽然好不容易招到了四名新社员，但却又卷入了四十五年前社长突然肄业的谜团之中。社长当年留下的名为「冰菓」的社刊，内里究竟隐藏了什么神秘的讯息呢……",
+		episodes: "22",
+		year: "2012",
+		genre: [
+			"推理",
+			"恋爱",
+			"日常",
+			"校园"
+		],
+		studio: "京都动画",
+		link: "https://www.bilibili.com/bangumi/media/md3398",
+		progress: "",
+		totalEpisodes: ""
 	}
 ];
 

8dc816066b043a92ce65cf29dc9279643990bc7d
余京 <2803673194@qq.com>
2026-08-08 18:17:54 +0800

    chore(editor): 批量更新 1 项

--- data/website.ts
+++ data/website.ts
@@ -5,11 +5,12 @@ const friendsData: FriendItem[] = [
 	{
 		id: 1,
 		title: "Mizuki Docs",
-		imgurl:
-			"https://q.qlogo.cn/headimg_dl?dst_uin=3231515355&spec=640&img_type=jpg",
+		imgurl: "https://q.qlogo.cn/headimg_dl?dst_uin=3231515355&spec=640&img_type=jpg",
 		desc: "Mizuki User Manual",
 		siteurl: "https://docs.mizuki.mysqil.com",
-		tags: ["技术"],
+		tags: [
+			"技术"
+		]
 	},
 	{
 		id: 2,
@@ -17,7 +18,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://ts3.tc.mm.bing.net/th/id/OIP-C.BHToo31Gbbw69wyXq4pqBQAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
 		desc: "Develop. Preview. Ship.",
 		siteurl: "https://vercel.com",
-		tags: ["技术"],
+		tags: [
+			"技术"
+		]
 	},
 	{
 		id: 3,
@@ -25,7 +28,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://tse3-mm.cn.bing.net/th/id/OIP-C.Lwbgu4eSpSz4gKe-F_k58gHaHa?w=167&h=180&c=7&r=0&o=7&pid=1.7&rm=3",
 		desc: "Where the world builds software",
 		siteurl: "https://github.com",
-		tags: ["技术"],
+		tags: [
+			"技术"
+		]
 	},
 	{
 		id: 3,
@@ -33,7 +38,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://hfyzdns.cn/logo.png",
 		desc: "合肥一中历史最悠久的科技社团之一。 零基础友好 · 技术驱动。从编程入门到 VR 开发，从游戏设计到 AI 实践，打造自由交流、学习、创造的平台。",
 		siteurl: "https://hfyzdns.cn",
-		tags: ["校园"],
+		tags: [
+			"校园"
+		]
 	},
 	{
 		id: 4,
@@ -41,7 +48,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://cfbed.sanyue.de/logo.png",
 		desc: "开源文件托管解决方案，安心存取，轻松分享",
 		siteurl: "https://cfbed.sanyue.de",
-		tags: ["技术"],
+		tags: [
+			"技术"
+		]
 	},
 	{
 		id: 5,
@@ -49,7 +58,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://www.boosds.cn/uploads/image/20260406/5572e4ad14fa6e2579205aa9048d2429.png",
 		desc: "免费分享百万级网盘资源,请输入准确影视名称进行搜索！",
 		siteurl: "https://www.boosds.cn",
-		tags: ["资源"],
+		tags: [
+			"资源"
+		]
 	},
 	{
 		id: 6,
@@ -57,7 +68,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://twikoo.js.org/twikoo-logo-home.png",
 		desc: "网站评论系统，简洁、安全、免费",
 		siteurl: "https://twikoo.js.org",
-		tags: ["技术"],
+		tags: [
+			"技术"
+		]
 	},
 	{
 		id: 7,
@@ -65,7 +78,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://ts1.tc.mm.bing.net/th/id/OIP-C.pxhC07XCuH4CfENun93MqwAAAA?r=0&rs=1&pid=ImgDetMain&o=7&rm=3",
 		desc: "一个免费&开源的音乐查找工具",
 		siteurl: "https://lxmusic.toside.cn",
-		tags: ["资源"],
+		tags: [
+			"资源"
+		]
 	},
 	{
 		id: 8,
@@ -73,7 +88,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://bee-reg-ab.imagency.cn/p/81985253f7a87ad615ee5ad6fffba283.png",
 		desc: "提供免费、稳定、高速的图片上传与外链服务平台",
 		siteurl: "https://www.beeimg.cn/",
-		tags: ["图床"],
+		tags: [
+			"图床"
+		]
 	},
 	{
 		id: 9,
@@ -81,7 +98,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://www.zhanlian.net/wp-content/uploads/2022/10/36e0f-www.cloudflare.com.png",
 		desc: "为您的网站提供免费的防护和加速",
 		siteurl: "https://dash.cloudflare.com/",
-		tags: ["技术"],
+		tags: [
+			"技术"
+		]
 	},
 	{
 		id: 10,
@@ -89,7 +108,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://tse2-mm.cn.bing.net/th/id/OIP-C.c7j-78QvJc2VrZ-j1t80FAAAAA?w=154&h=155&c=7&r=0&o=7&pid=1.7&rm=3",
 		desc: "怀天下抱负，做未来主人。",
 		siteurl: "http://www.hfyz.net/sy/index.html",
-		tags: ["校园"],
+		tags: [
+			"校园"
+		]
 	},
 	{
 		id: 11,
@@ -97,7 +118,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://www.pighub.top/images/%E7%88%B1%E4%B8%8A%E4%B8%80%E5%8F%AA%E5%B0%8F%E7%8C%AA%EF%BC%9F.jpg",
 		desc: "最大的猪猪图片网站",
 		siteurl: "https://www.pighub.top/",
-		tags: ["娱乐"],
+		tags: [
+			"娱乐"
+		]
 	},
 	{
 		id: 12,
@@ -105,7 +128,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://tse3-mm.cn.bing.net/th/id/OIP-C.YrNMwDyCI7RRbq5_NIVNhQAAAA?w=131&h=150&c=7&r=0&o=7&pid=1.7&rm=3",
 		desc: "世界上最大的电子图书馆。自由访问知识和文化。",
 		siteurl: "https://zh.kid1412.by/",
-		tags: ["资源"],
+		tags: [
+			"资源"
+		]
 	},
 	{
 		id: 13,
@@ -113,7 +138,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://weavatar.com/avatar/50abbf204520fa0a98fdaab4bc98a10f",
 		desc: "将 GitHub 链接转换为多区域加速链接，解决 GitHub 访问慢、下载失败等问题",
 		siteurl: "https://gh-proxy.com/",
-		tags: ["工具"],
+		tags: [
+			"工具"
+		]
 	},
 	{
 		id: 14,
@@ -121,7 +148,9 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://img.yujingblog.top/file/1785690853054_logo.png",
 		desc: "我的个人图床",
 		siteurl: "https://img.yujingblog.top",
-		tags: ["图床"],
+		tags: [
+			"图床"
+		]
 	},
 	{
 		id: 15,
@@ -129,8 +158,172 @@ const friendsData: FriendItem[] = [
 		imgurl: "https://haowallpaper.com/favicon.ico",
 		desc: "免费4K高清壁纸-电脑背景图片-Mac壁纸网站",
 		siteurl: "https://haowallpaper.com/",
-		tags: ["资源"],
+		tags: [
+			"资源"
+		]
 	},
+	{
+		title: "DeepSeek | 深度求索",
+		imgurl: "https://a.favicon.im/www.deepseek.com",
+		desc: "deepseek探索未至之境",
+		siteurl: "https://www.deepseek.com/",
+		tags: [
+			"AI"
+		]
+	},
+	{
+		title: "Favicon.im: 即时网站图标获取器",
+		imgurl: "https://a.favicon.im/favicon.im",
+		desc: "Favicon.im是一个简单高效的服务，允许您获取任何网站的图标（网站图标）。 只需提供域名，我们就 …",
+		siteurl: "https://favicon.im/zh",
+		tags: [
+			"工具"
+		]
+	},
+	{
+		title: "豆包",
+		imgurl: "https://a.favicon.im/www.doubao.com",
+		desc: "豆包 - 字节跳动旗下 AI 智能助手",
+		siteurl: "https://www.doubao.com",
+		tags: [
+			"AI"
+		]
+	},
+	{
+		title: "千问",
+		imgurl: "https://a.favicon.im/www.qianwen.com",
+		desc: "千问-阿里Qwen最新模型体验 - 界面简洁，交互流畅清爽",
+		siteurl: "https://www.qianwen.com/",
+		tags: [
+			"AI"
+		]
+	},
+	{
+		title: "skills mp",
+		imgurl: "https://a.favicon.im/skillsmp.com",
+		desc: "看人们正在教 Claude、Codex 和其他 AI 智能体做些什么。",
+		siteurl: "https://skillsmp.com/zh",
+		tags: [
+			"AI工具"
+		]
+	},
+	{
+		title: "图床 | StarDots",
+		imgurl: "https://a.favicon.im/dashboard.stardots.io",
+		desc: "图像云存储。\n图片托管，让你的图片公开访问。\n安全数据，完全免费。",
+		siteurl: "https://dashboard.stardots.io/",
+		tags: [
+			"图床"
+		]
+	},
+	{
+		title: "PromptPilot",
+		imgurl: "https://a.favicon.im/promptpilot.volcengine.com",
+		desc: "PromptPilot是火山引擎推出的AI提示词优化平台，能将用户模糊的想法转化为AI能精准执行的专业指令。本文全面 …",
+		siteurl: "https://promptpilot.volcengine.com/",
+		tags: [
+			"AI工具"
+		]
+	},
+	{
+		title: "Iconify",
+		imgurl: "https://a.favicon.im/icon-sets.iconify.design",
+		desc: "开源图标集的家园",
+		siteurl: "https://icon-sets.iconify.design/",
+		tags: [
+			"前端组件"
+		]
+	},
+	{
+		title: "Imagio",
+		imgurl: "https://a.favicon.im/image.oblivionis.net",
+		desc: "描述你想生成的图片，或附加图片进行编辑……",
+		siteurl: "https://image.oblivionis.net/",
+		tags: [
+			"AI工具"
+		]
+	},
+	{
+		title: "One Page Love",
+		imgurl: "https://a.favicon.im/onepagelove.com",
+		desc: "一页网站，精心策划。",
+		siteurl: "https://onepagelove.com/",
+		tags: [
+			"前端组件"
+		]
+	},
+	{
+		title: "坐标拾取器 | 高德地图API",
+		imgurl: "https://a.favicon.im/lbs.amap.com",
+		desc: "获取足迹页面所需的精确经纬",
+		siteurl: "https://lbs.amap.com/tools/picker",
+		tags: [
+			"工具"
+		]
+	},
+	{
+		title: "AGE动漫",
+		imgurl: "https://a.favicon.im/www.agedm.io",
+		desc: "免费动漫资源聚合站",
+		siteurl: "https://www.agedm.io/",
+		tags: [
+			"资源"
+		]
+	},
+	{
+		title: "MANHWATOP",
+		imgurl: "https://a.favicon.im/manhwatop.com",
+		desc: "收录漫画资源",
+		siteurl: "https://manhwatop.com/",
+		tags: [
+			"资源"
+		]
+	},
+	{
+		title: "免费在线抠图",
+		imgurl: "https://a.favicon.im/www.koukoutu.com",
+		desc: "无需上传的在线图像抠图工具",
+		siteurl: "https://www.koukoutu.com/removebgtool/all",
+		tags: [
+			"工具"
+		]
+	},
+	{
+		title: "imagesTool",
+		imgurl: "https://a.favicon.im/imagestool.com",
+		desc: "无需上传文件也可在线处理图片",
+		siteurl: "https://imagestool.com/zh_CN/",
+		tags: [
+			"工具"
+		]
+	},
+	{
+		title: "Itdog-在线测速",
+		imgurl: "https://a.favicon.im/www.itdog.cn",
+		desc: "在线网络工具箱，网站测速",
+		siteurl: "https://www.itdog.cn/",
+		tags: [
+			"工具"
+		]
+	},
+	{
+		title: "免费在线视频压缩工具",
+		imgurl: "https://a.favicon.im/videocompress.io",
+		desc: "免费在线视频压缩工具，支持 MP4、MOV、WebM 等格式。免费在线压缩视频文件大小，在画质和体积之间取得合适平衡。无需安装软件，上传后即可压缩下载。",
+		siteurl: "https://videocompress.io/zh-cn",
+		tags: [
+			"工具"
+		]
+	},
+	{
+		title: "Qwerty Learner",
+		imgurl: "https://qwerty.kaiyi.cool/",
+		desc: "键盘工作者单词记忆软件",
+		siteurl: "https://qwerty.kaiyi.cool/",
+		tags: [
+			"学习"
+		]
+	}
 ];
 export function getFriendsList(): FriendItem[] {
 	return friendsData;

1aec1ddcc6a8c451a63d0b2fe2ec37ded6ead459
余京 <2803673194@qq.com>
2026-08-13 06:58:33 +0800

    chore(editor): 批量更新 1 项

--- data/diary.ts
+++ data/diary.ts
@@ -16,131 +16,78 @@ export interface DiaryItem {
 // 日记数据
 const diaryData: DiaryItem[] = [
 	{
-		id: 1,
-		content: "它老冯的！这个博客写了好几天了。今天终于写到日记了，算鸟，当学MarkDown了。",
-		date: "2026-07-26T20:58:00+08:00",
+		content: "记录，第一次被攻击，虽然只是储存xxs，我只限输入30字，想着应该拿不到我的Cookie就没事。就今天输密码半天进不去，我还真以为拿到了，一检查才发现是之前误删worker，变量跟着丢了，忘加了😒\n但因为textContent 是空的或和原文不一致，管理员还删不掉，也确实被折腾了😅",
+		date: "2026-08-11T15:41:00+08:00",
 		images: [
-			"https://bee-reg-ab.imagency.cn/p/0579b27512a43f6f5ecdfdd75d363f16.png"
+			"https://img.yujingblog.top/file/1786432856824_image.png"
 		],
-		location: "家里",
-		mood: "崩溃",
+		location: "",
+		mood: "震惊",
 		tags: [
-			"博客搭建"
-		]
+			"技术"
+		],
+		imgMode: "mode-fill",
+		id: 18
 	},
 	{
-		id: 2,
-		content: "OK呀我也是起床了好吧。",
-		date: "2026-07-27T12:39:01+08:00",
+		content: "睡不着ing…",
+		date: "2026-08-11T04:29:00+08:00",
 		images: [],
-		mood: "无语",
-		tags: [
-			"日常"
-		]
-	},
-	{
-		id: 3,
-		content: "我的token呀!/(ㄒoㄒ)/~~,这复刻个GitHub上的开源项目这么烧token,用了ClawX和CodeX,这个CodeX现在怎么放在Chat GTP里了，没有想象的好用（呃有可能是因为换了模型吧）总之白期待了，我的token啊，早知道不偷懒了，呜呜呜😒",
-		date: "2026-07-27T19:06:01+08:00",
-		images: [
-			"https://bee-reg-ab.imagency.cn/p/a425b75dcb73d3b23eed6309a5f84ae1.png"
-		],
-		mood: "心痛",
-		tags: [
-			"博客搭建"
-		]
-	},
-	{
-		id: 4,
-		content: "删了小两百个好友，十来天了，还是只有四个人发现，轮存在感这一块👍。",
-		date: "2026-07-30T14:38:31+08:00",
-		images: [
-			"https://img.yujingblog.top/file/1785928043492_屏幕截图_2026-07-24_152153.webp"
-		],
-		mood: "扎心➳♥゛",
-		tags: [
-			"碎碎念"
-		],
-		imgMode: "mode-mid"
-	},
-	{
-		id: 5,
-		content: "都走了安静多了，我好像还能活了😗",
-		date: "2026-07-30T20:38:31+08:00",
-		images: [],
-		mood: "轻松",
-		tags: [
-			"碎碎念"
-		]
-	},
-	{
-		id: 6,
-		content: "一个人",
-		date: "2026-07-31T00:12:05+08:00",
-		images: [],
-		mood: "轻松",
-		tags: [
-			"碎碎念"
-		]
-	},
-	{
-		id: 7,
-		content: "谁懂通宵两天写出来的屎山代码都跑不起来，有全滚回到一开始的版本",
-		date: "2026-07-31T01:59:05+08:00",
-		images: [
-			"https://tse4-mm.cn.bing.net/th/id/OIP-C.FAuf6qKdlov89LMj7PqQJAAAAA?w=170&h=180&c=7&r=0&o=7&pid=1.7&rm=3"
-		],
-		mood: "命苦",
-		tags: [
-			"博客搭建"
-		],
-		imgMode: "mode-grid"
-	},
-	{
-		id: 8,
-		content: "买了个lived2d模型，0.15￥呢。原本准备放到博客里，但这个好像是转卖的，算了我支持正版(咳咳！绝对不是因为不会去水印 嗷┗|｀O′|┛~~)",
-		date: "2026-08-01T03:59:05+08:00",
-		images: [
-			"https://img.yujingblog.top/file/1785786132107_屏幕截图_2026-08-01_185526.webp"
-		],
-		mood: "呃(⊙﹏⊙)",
+		location: "床",
+		mood: "无聊",
 		tags: [
 			"日常"
 		],
-		imgMode: "mode-mid"
+		imgMode: "",
+		id: 17
 	},
 	{
-		id: 9,
-		content: "看板娘终于升级好了，喵🐾 ",
-		date: "2026-08-02T02:55:05+08:00",
+		content: "依旧，我“起”这么早",
+		date: "2026-08-10T05:07:00+08:00",
 		images: [],
-		location: "安徽",
-		mood: "舒服",
-		tags: [
-			"博客搭建"
-		]
-	},
-	{
-		id: 10,
-		content: "想要变成这样，快来留言！",
-		date: "2026-08-04T02:58:05+08:00",
-		images: [
-			"https://img.yujingblog.top/file/1785781799608_image.webp"
-		],
-		mood: "羡慕",
-		tags: [
-			"博客搭建"
-		]
-	},
-	{
-		id: 11,
-		content: "这次是彻底穿了😴",
-		date: "2026-08-05T06:00:00+08:00",
-		images: [],
-		mood: "疲惫",
+		location: "",
+		mood: "难评",
 		tags: [
 			"日常"
-		]
+		],
+		imgMode: "",
+		id: 16
+	},
+	{
+		content: "AI漫剧误我啊！\n",
+		date: "2026-08-09T05:42:00+08:00",
+		images: [],
+		location: "家",
+		mood: "累",
+		tags: [
+			"日常"
+		],
+		imgMode: "",
+		id: 15
+	},
+	{
+		id: 14,
+		content: "666，一直以为是我网站没优化好，没做优选，没用CDN，结果今天用我🐎手机，才发现……\ntm的是设备的问题，还只是14P，秒进3D音乐可视化页面，都不带加载的，我哭死了┭┮﹏┭┮\n原来只是我设备都太烂了……",
+		date: "2026-08-08T18:18:00+08:00",
+		images: [],
+		location: "家",
+		mood: "哭死",
+		tags: [
+			"碎碎念"
+		],
+		imgMode: ""
+	},
+	{
+		id: 13,
+		content: "博客基本已经成熟了",
+		date: "2026-08-08T15:28:00+08:00",
+		images: [],
+		location: "",
+		mood: "放松",
+		tags: [
+			"博客搭建"
+		],
+		imgMode: ""
 	},
 	{
 		id: 12,
@@ -157,78 +104,143 @@ const diaryData: DiaryItem[] = [
 		imgMode: "mode-grid"
 	},
 	{
-		id: 13,
-		content: "博客基本已经成熟了",
-		date: "2026-08-08T15:28:00+08:00",
+		id: 11,
+		content: "这次是彻底穿了😴",
+		date: "2026-08-05T06:00:00+08:00",
 		images: [],
-		location: "",
-		mood: "放松",
+		mood: "疲惫",
+		tags: [
+			"日常"
+		]
+	},
+	{
+		id: 10,
+		content: "想要变成这样，快来留言！",
+		date: "2026-08-04T02:58:05+08:00",
+		images: [
+			"https://img.yujingblog.top/file/1785781799608_image.webp"
+		],
+		mood: "羡慕",
+		tags: [
+			"博客搭建"
+		]
+	},
+	{
+		id: 9,
+		content: "看板娘终于升级好了，喵🐾 ",
+		date: "2026-08-02T02:55:05+08:00",
+		images: [],
+		location: "安徽",
+		mood: "舒服",
+		tags: [
+			"博客搭建"
+		]
+	},
+	{
+		id: 8,
+		content: "买了个lived2d模型，0.15￥呢。原本准备放到博客里，但这个好像是转卖的，算了我支持正版(咳咳！绝对不是因为不会去水印 嗷┗|｀O′|┛~~)",
+		date: "2026-08-01T03:59:05+08:00",
+		images: [
+			"https://img.yujingblog.top/file/1785786132107_屏幕截图_2026-08-01_185526.webp"
+		],
+		mood: "呃(⊙﹏⊙)",
+		tags: [
+			"日常"
+		],
+		imgMode: "mode-mid"
+	},
+	{
+		id: 7,
+		content: "谁懂通宵两天写出来的屎山代码都跑不起来，有全滚回到一开始的版本",
+		date: "2026-07-31T01:59:05+08:00",
+		images: [
+			"https://tse4-mm.cn.bing.net/th/id/OIP-C.FAuf6qKdlov89LMj7PqQJAAAAA?w=170&h=180&c=7&r=0&o=7&pid=1.7&rm=3"
+		],
+		mood: "命苦",
 		tags: [
 			"博客搭建"
 		],
-		imgMode: ""
+		imgMode: "mode-grid"
 	},
 	{
-		id: 14,
-		content: "666，一直以为是我网站没优化好，没做优选，没用CDN，结果今天用我🐎手机，才发现……\ntm的是设备的问题，还只是14P，秒进3D音乐可视化页面，都不带加载的，我哭死了┭┮﹏┭┮\n原来只是我设备都太烂了……",
-		date: "2026-08-08T18:18:00+08:00",
+		id: 6,
+		content: "一个人",
+		date: "2026-07-31T00:12:05+08:00",
 		images: [],
-		location: "家",
-		mood: "哭死",
+		mood: "轻松",
+		tags: [
+			"碎碎念"
+		]
+	},
+	{
+		id: 5,
+		content: "都走了安静多了，我好像还能活了😗",
+		date: "2026-07-30T20:38:31+08:00",
+		images: [],
+		mood: "轻松",
+		tags: [
+			"碎碎念"
+		]
+	},
+	{
+		id: 4,
+		content: "删了小两百个好友，十来天了，还是只有四个人发现，轮存在感这一块👍。",
+		date: "2026-07-30T14:38:31+08:00",
+		images: [
+			"https://img.yujingblog.top/file/1785928043492_屏幕截图_2026-07-24_152153.webp"
+		],
+		mood: "扎心➳♥゛",
 		tags: [
 			"碎碎念"
 		],
-		imgMode: ""
+		imgMode: "mode-mid"
 	},
 	{
-		content: "AI漫剧误我啊！\n",
-		date: "2026-08-09T05:42:00+08:00",
-		images: [],
-		location: "家",
-		mood: "累",
-		tags: [
-			"日常"
-		],
-		imgMode: "",
-		id: 15
-	},
-	{
-		content: "依旧，我“起”这么早",
-		date: "2026-08-10T05:07:00+08:00",
-		images: [],
-		location: "",
-		mood: "难评",
-		tags: [
-			"日常"
-		],
-		imgMode: "",
-		id: 16
-	},
-	{
-		content: "睡不着ing…",
-		date: "2026-08-11T04:29:00+08:00",
-		images: [],
-		location: "床",
-		mood: "无聊",
-		tags: [
-			"日常"
-		],
-		imgMode: "",
-		id: 17
-	},
-	{
-		content: "记录，第一次被攻击，虽然只是储存xxs，我只限输入30字，想着应该拿不到我的Cookie就没事。就今天输密码半天进不去，我还真以为拿到了，一检查才发现是之前误删worker，变量跟着丢了，忘加了😒\n但因为textContent 是空的或和原文不一致，管理员还删不掉，也确实被折腾了😅",
-		date: "2026-08-11T15:41:00+08:00",
+		id: 3,
+		content: "我的token呀!/(ㄒoㄒ)/~~,这复刻个GitHub上的开源项目这么烧token,用了ClawX和CodeX,这个CodeX现在怎么放在Chat GTP里了，没有想象的好用（呃有可能是因为换了模型吧）总之白期待了，我的token啊，早知道不偷懒了，呜呜呜😒",
+		date: "2026-07-27T19:06:01+08:00",
 		images: [
-			"https://img.yujingblog.top/file/1786432856824_image.png"
+			"https://bee-reg-ab.imagency.cn/p/a425b75dcb73d3b23eed6309a5f84ae1.png"
 		],
-		location: "",
-		mood: "震惊",
+		mood: "心痛",
 		tags: [
-			"技术"
+			"博客搭建"
+		]
+	},
+	{
+		id: 2,
+		content: "OK呀我也是起床了好吧。",
+		date: "2026-07-27T12:39:01+08:00",
+		images: [],
+		mood: "无语",
+		tags: [
+			"日常"
+		]
+	},
+	{
+		id: 1,
+		content: "它老冯的！这个博客写了好几天了。今天终于写到日记了，算鸟，当学MarkDown了。",
+		date: "2026-07-26T20:58:00+08:00",
+		images: [
+			"https://bee-reg-ab.imagency.cn/p/0579b27512a43f6f5ecdfdd75d363f16.png"
 		],
-		imgMode: "mode-fill",
-		id: 18
+		location: "家里",
+		mood: "崩溃",
+		tags: [
+			"博客搭建"
+		]
+	},
+	{
+		content: "I ‘m back",
+		date: "2026-08-13T06:58:01+08:00",
+		images: [],
+		location: "北京",
+		mood: "",
+		tags: [
+			"日常"
+		],
+		imgMode: "",
+		id: 19
 	}
 ];
 

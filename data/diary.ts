cc730ced0d4fa3991ec9e1a1459e93eee4926a4a
余京 <2803673194@qq.com>
2026-09-25 21:19:44 +0800

    chore(editor): 批量更新 1 项

--- data/diary.ts
+++ data/diary.ts
@@ -16,6 +16,16 @@ export interface DiaryItem {
 // 日记数据
 const diaryData: DiaryItem[] = [
 	{
+		content: "Github牛逼不解释，原来我对Github的开发仅为1%",
+		date: "2026-09-24T21:25:51+08:00",
+		images: [],
+		location: "",
+		mood: "",
+		tags: [],
+		imgMode: "",
+		id: 42
+	},
+	{
 		content: "给我气哭了，我妈花钱找人清电脑，结果把我BCD引导删了，看不了机，搞了我半天",
 		date: "2026-09-18T22:35:04+08:00",
 		images: [],
@@ -547,14 +557,14 @@ const diaryData: DiaryItem[] = [
 		id: 24
 	},
 	{
-		content: "Github牛逼不解释，原来我对Github的开发仅为1%",
-		date: "2026-09-24T21:25:51+08:00",
+		content: "呵呵(￣、￣)",
+		date: "2026-09-25T21:18:02+08:00",
 		images: [],
 		location: "",
-		mood: "",
+		mood: "死了",
 		tags: [],
 		imgMode: "",
-		id: 42
+		id: 43
 	}
 ];
 
